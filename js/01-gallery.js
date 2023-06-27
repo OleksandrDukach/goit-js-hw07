fd47598f954fad4be8bc4cc051a6cea2681b29a0
OleksandrDukach <umnick36@gmail.com>
2023-06-27 15:31:13 +0300

    create

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,50 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryPrimeEl = document.querySelector(".gallery");
+
+const newGallery = galleryItems.map(
+  ({ preview, original, description }) => 
+    
+    
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`).join("");
+
+let instance
+  
+galleryPrimeEl.insertAdjacentHTML("beforeend", newGallery)
+
+galleryPrimeEl.addEventListener("click", handleClick)
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(
+    `<img 
+    src="${event.target.dataset.source}"
+    alt = ${event.target.description}/>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", pressModalKey)
+      },
+      onClose: (instance) => document.removeEventListener("keydown", pressModalKey)
+    });
+  instance.show()
+}
+function pressModalKey(evt) {
+  if (evt.key === "Escape") {
+    instance.close()
+  }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryPrimeEl = document.querySelector(".gallery");
+
+const newGallery = galleryItems.map(
+  ({ preview, original, description }) => 
+        
+  `<li
+   class="gallery__item">
+   <a class="gallery__link" href="${original}">
+   <img class="gallery__image"
+   src="${preview}"
+   alt="${description}" />
+   </a>
+</li>`).join("");
+
+galleryPrimeEl.insertAdjacentHTML("beforeend", newGallery);
+
+galleryPrimeEl.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+}
+
+const galleryScroll = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
